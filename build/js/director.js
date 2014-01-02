45081c8c36aa78aac0a5963aee265c147a7e805b
Eric <Ericrius1@gmail.com>
2014-01-02 02:03:58 -0800

    scene transition stil not quite right

--- build/js/director.js
+++ build/js/director.js
@@ -3,8 +3,6 @@
 
   FW.Director = Director = (function() {
     function Director() {
-      var startTime, totalTime,
-        _this = this;
       this.showHasBegun = false;
       this.colorChangeTime = 50;
       this.skyColor = new THREE.Color();
@@ -13,60 +11,18 @@
       this.controls.enabled = false;
       this.controls.zoomSpeed = 0.5;
       this.controls.rotateSpeed = 0.5;
-      startTime = Date.now();
-      totalTime = 154700;
-      FW.scene1 = {
-        startSkyHue: 0.12,
-        startSkyLight: 0.5,
-        startTime: startTime,
-        totalTime: totalTime,
-        endTime: startTime + totalTime
-      };
-      FW.scene2 = {
-        startTime: FW.scene1.endTime,
-        songPoint: 154700,
-        endTime: FW.scene1.endTime + 101000,
-        totalTime: 101000,
-        camSpeed: 0.0,
-        camAcceleration: 0.0002,
-        beatInterval: 3540
-      };
+      this.startSkyHue = 0.12;
+      this.startSkyLight = 0.5;
       this.currentScene = FW.scene1;
-      this.skyColor.setHSL(FW.scene1.startSkyHue, 0.86, FW.scene1.startSkyLight);
+      this.skyColor.setHSL(this.startSkyHue, 0.86, this.startSkyLight);
       FW.renderer.setClearColor(this.skyColor);
       this.skyLagFactor = 1.7;
-      FW.scene1.update = function() {
-        _this.hue = map(_this.currentTime, FW.scene1.startTime, FW.scene1.endTime, FW.scene1.startSkyHue, 0);
-        _this.light = map(_this.currentTime, FW.scene1.startTime, FW.scene1.endTime, FW.scene1.startSkyLight, 0.2);
-        _this.skyColor.setHSL(_this.hue, 0.86, _this.light);
-        FW.renderer.setClearColor(_this.skyColor);
-        if (FW.sunLight.position.y > FW.sunFinalHeight) {
-          FW.mySun.update();
-        }
-        FW.myCamera.scene1Update();
-        if (_this.currentTime > FW.scene1.endTime) {
-          return _this.triggerScene2();
-        }
-      };
-      FW.scene2.update = function() {
-        var light;
-        FW.fireflies.tick();
-        FW.myCamera.scene2Update();
-        light = map(_this.currentTime, FW.scene2.startTime, FW.scene2.endTime, 0.2, 0);
-        _this.skyColor.setHSL(_this.hue, 0.86, light);
-        return FW.renderer.setClearColor(_this.skyColor);
-      };
-      this.currentScene = FW.scene1;
     }
 
-    Director.prototype.beginShow = function() {
-      return this.showHasBegun = true;
-    };
-
     Director.prototype.update = function() {
-      this.currentTime = Date.now();
       if (!this.frozen) {
         if (this.showHasBegun) {
+          this.currentTime = Date.now();
           return this.currentScene.update();
         }
       } else {
@@ -85,6 +41,51 @@
       this.frozen = !this.frozen;
       this.controls.enabled = !this.controls.enabled;
       return this.controls.target.z = FW.camera.position.z - 30;
+    };
+
+    Director.prototype.beginShow = function() {
+      var startTime, totalTime,
+        _this = this;
+      startTime = Date.now();
+      totalTime = 154700;
+      FW.scene1 = {
+        startTime: startTime,
+        totalTime: totalTime,
+        endTime: startTime + totalTime
+      };
+      totalTime = 101000;
+      FW.scene2 = {
+        startTime: FW.scene1.endTime,
+        songPoint: 154700,
+        endTime: FW.scene1.endTime + totalTime,
+        totalTime: totalTime,
+        camSpeed: 0.0,
+        camAcceleration: 0.0002,
+        beatInterval: 3540
+      };
+      this.showHasBegun = true;
+      FW.scene1.update = function() {
+        _this.hue = map(_this.currentTime, FW.scene1.startTime, FW.scene1.endTime, _this.startSkyHue, 0);
+        _this.light = map(_this.currentTime, FW.scene1.startTime, FW.scene1.endTime, _this.startSkyLight, 0.2);
+        _this.skyColor.setHSL(_this.hue, 0.86, _this.light);
+        FW.renderer.setClearColor(_this.skyColor);
+        if (FW.sunLight.position.y > FW.sunFinalHeight) {
+          FW.mySun.update();
+        }
+        FW.myCamera.scene1Update();
+        if (_this.currentTime > FW.scene1.endTime) {
+          return _this.triggerScene2();
+        }
+      };
+      FW.scene2.update = function() {
+        var light;
+        FW.fireflies.tick();
+        FW.myCamera.scene2Update();
+        light = map(_this.currentTime, FW.scene2.startTime, FW.scene2.endTime, 0.2, 0);
+        _this.skyColor.setHSL(_this.hue, 0.86, light);
+        return FW.renderer.setClearColor(_this.skyColor);
+      };
+      return this.currentScene = FW.scene1;
     };
 
     return Director;
--- src/director.coffee
+++ src/director.coffee
@@ -11,57 +11,22 @@ FW.Director = class Director
     @controls.zoomSpeed = 0.5
     @controls.rotateSpeed = 0.5
 
-    #SCENES
-    startTime = Date.now()
-    totalTime = 154700
-    FW.scene1 =
-      startSkyHue: 0.12
-      startSkyLight: 0.5
-      startTime: startTime
-      totalTime: totalTime
-      endTime: startTime + totalTime
-    FW.scene2 = 
-      startTime: FW.scene1.endTime
-      songPoint: 154700
-      endTime: FW.scene1.endTime + 101000
-      totalTime: 101000
-      camSpeed: 0.0
-      camAcceleration: 0.0002
-      beatInterval: 3540
-    #first beat: 154700
-    #second beat: 158260
+    @startSkyHue = 0.12
+    @startSkyLight = 0.5
+
+   
 
     @currentScene = FW.scene1
-    @skyColor.setHSL FW.scene1.startSkyHue, 0.86, FW.scene1.startSkyLight
+    @skyColor.setHSL @startSkyHue, 0.86, @startSkyLight
     FW.renderer.setClearColor @skyColor
     
     @skyLagFactor = 1.7
-    FW.scene1.update = =>
-      @hue = map(@currentTime, FW.scene1.startTime, FW.scene1.endTime,  FW.scene1.startSkyHue, 0 )
-      @light = map(@currentTime, FW.scene1.startTime, FW.scene1.endTime,  FW.scene1.startSkyLight, 0.2 )
-      @skyColor.setHSL @hue, 0.86, @light
-      FW.renderer.setClearColor @skyColor
-      if FW.sunLight.position.y > FW.sunFinalHeight
-        FW.mySun.update()  
-      FW.myCamera.scene1Update()
-      if @currentTime > FW.scene1.endTime
-        @triggerScene2()
-    FW.scene2.update = =>
-      FW.fireflies.tick()
-      FW.myCamera.scene2Update()
-      light = map(@currentTime, FW.scene2.startTime, FW.scene2.endTime,  0.2, 0 )
-      @skyColor.setHSL @hue, 0.86, light
-      FW.renderer.setClearColor @skyColor
-
-    @currentScene = FW.scene1
-
-
-  beginShow: ->
-    @showHasBegun = true
+    
   update: ->
-    @currentTime = Date.now()
     if !@frozen
       if @showHasBegun
+        #only update time if we are running the show!
+        @currentTime = Date.now()
         @currentScene.update()
     else
       @controls.update()
@@ -79,7 +44,46 @@ FW.Director = class Director
     @controls.target.z = FW.camera.position.z - 30    
 
 
+  beginShow: ->
+    #Inititalize Scenes
+    startTime = Date.now()
+    totalTime = 154700
+    FW.scene1 =
+      startTime: startTime
+      totalTime: totalTime
+      endTime: startTime + totalTime
+    totalTime = 101000
+    #BEWARE of overwriting total time!!
+    FW.scene2 = 
+      startTime: FW.scene1.endTime
+      songPoint: 154700
+      endTime: FW.scene1.endTime + totalTime
+      totalTime: totalTime
+      camSpeed: 0.0
+      camAcceleration: 0.0002
+      beatInterval: 3540
+    #first beat: 154700
+    #second beat: 158260
+    @showHasBegun = true
 
+    FW.scene1.update = =>
+      @hue = map(@currentTime, FW.scene1.startTime, FW.scene1.endTime,  @startSkyHue, 0 )
+      @light = map(@currentTime, FW.scene1.startTime, FW.scene1.endTime,  @startSkyLight, 0.2 )
+      @skyColor.setHSL @hue, 0.86, @light
+      FW.renderer.setClearColor @skyColor
+      if FW.sunLight.position.y > FW.sunFinalHeight
+        FW.mySun.update()  
+      FW.myCamera.scene1Update()
+      if @currentTime > FW.scene1.endTime
+        @triggerScene2()
+    FW.scene2.update = =>
+      FW.fireflies.tick()
+      FW.myCamera.scene2Update()
+      light = map(@currentTime, FW.scene2.startTime, FW.scene2.endTime,  0.2, 0 )
+      @skyColor.setHSL @hue, 0.86, light
+      FW.renderer.setClearColor @skyColor
+
+    @currentScene = FW.scene1
 
 
 
