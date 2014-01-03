a2aa648a687cfe87cd467005f80d5550b23a1a49
Eric <Ericrius1@gmail.com>
2014-01-03 10:52:05 -0800

    epic

--- build/js/sun.js
+++ build/js/sun.js
@@ -3,38 +3,42 @@
 
   FW.Sun = Sun = (function() {
     function Sun() {
-      this.startHue = 0.08;
-      this.startLight = 0.6;
-      FW.sunStartingHeight = 1200;
-      this.initialScale = 380;
       this.sunColor = new THREE.Color();
-      this.startIntensity = 7;
-      this.endIntensity = 2;
       this.startHue = 0.08;
       this.endHue = 0.00;
       this.startLight = 0.6;
       this.endLight = 0.35;
+      this.startIntensity = 5;
+      this.endIntensity = 2;
       this.startScale = 300;
       this.endScale = 600;
+      this.startHeight = 1200;
+      this.endHeight = -613;
       this.sunLight = new THREE.PointLight(0xffffff, this.startingIntensity, 10000);
-      this.sunLight.position = new THREE.Vector3(0, FW.sunStartingHeight, FW.width);
+      this.sunLight.position = new THREE.Vector3(0, this.startHeight, FW.width);
       FW.scene.add(this.sunLight);
       this.sunGeo = new THREE.SphereGeometry(1, 100, 100);
       this.material = new THREE.MeshBasicMaterial();
       this.sunMesh = new THREE.Mesh(this.sunGeo, this.material);
-      this.sunMesh.position = new THREE.Vector3(0, FW.sunStartingHeight, -FW.width);
-      this.sunMesh.scale.set(this.initialScale, this.initialScale, this.initialScale);
+      this.sunMesh.position = new THREE.Vector3(0, this.startHeight, -FW.width);
+      this.sunMesh.scale.set(this.startScale, this.startScale, this.startScale);
       FW.scene.add(this.sunMesh);
       this.sunColor.setHSL(this.startHue, 0.86, this.startLight);
       this.sunMesh.material.color = this.sunColor;
       this.sunLight.color = this.sunColor;
+      this.sunLight.intensity = this.startIntensity;
     }
 
     Sun.prototype.update = function() {
-      var currentTime, scale;
+      var currentTime, hue, light, scale;
       currentTime = Date.now();
+      this.sunMesh.position.y = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, this.startHeight, this.endHeight);
       scale = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, this.startScale, this.endScale);
-      return this.sunMesh.scale.set(scale, scale, scale);
+      this.sunMesh.scale.set(scale, scale, scale);
+      this.sunLight.intensity = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, this.startIntensity, this.endIntensity);
+      hue = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, this.startHue, this.endHue);
+      light = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, this.startLight, this.endLight);
+      return this.sunColor.setHSL(hue, 0.9, light);
     };
 
     return Sun;
--- src/sun.coffee
+++ src/sun.coffee
@@ -4,46 +4,51 @@ FW.Sun = class Sun
   constructor: ()->
 
 
-    @startHue = 0.08
-    @startLight = 0.6
-    FW.sunStartingHeight = 1200
-    @initialScale = 380
     @sunColor = new THREE.Color()
-
-    @startIntensity = 7
-    @endIntensity = 2
     @startHue = 0.08
     @endHue = 0.00
     @startLight = 0.6
     @endLight = 0.35
 
+    @startIntensity = 5
+    @endIntensity = 2
+
     @startScale = 300
     @endScale = 600
     
+    @startHeight = 1200
+    @endHeight = -613
     #LIGHT
     @sunLight = new THREE.PointLight(0xffffff, @startingIntensity, 10000)
-    @sunLight.position = new THREE.Vector3 0, FW.sunStartingHeight, FW.width
+    @sunLight.position = new THREE.Vector3 0, @startHeight, FW.width
     FW.scene.add @sunLight
 
     @sunGeo = new THREE.SphereGeometry(1, 100, 100)
     @material = new THREE.MeshBasicMaterial()
     @sunMesh = new THREE.Mesh @sunGeo, @material
-    @sunMesh.position = new THREE.Vector3 0, FW.sunStartingHeight, -FW.width
-    @sunMesh.scale.set @initialScale, @initialScale, @initialScale
+    @sunMesh.position = new THREE.Vector3 0, @startHeight, -FW.width
+    @sunMesh.scale.set @startScale, @startScale, @startScale
     FW.scene.add @sunMesh
 
     @sunColor.setHSL @startHue, 0.86, @startLight
     @sunMesh.material.color = @sunColor
     @sunLight.color = @sunColor
+    @sunLight.intensity = @startIntensity
 
 
   update : ->
     currentTime = Date.now()
+
+    @sunMesh.position.y = map currentTime, FW.scene1.startTime, FW.scene1.endTime, @startHeight, @endHeight
     
     scale = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, @startScale, @endScale)
     @sunMesh.scale.set scale, scale, scale
 
-    # @sunLight.intensity = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, @startIntensity, @endIntensity)
+    @sunLight.intensity = map(currentTime, FW.scene1.startTime, FW.scene1.endTime, @startIntensity, @endIntensity)
+
+    hue = map currentTime, FW.scene1.startTime, FW.scene1.endTime, @startHue, @endHue
+    light = map currentTime, FW.scene1.startTime, FW.scene1.endTime, @startLight, @endLight
+    @sunColor.setHSL hue, 0.9, light
 
 
 
